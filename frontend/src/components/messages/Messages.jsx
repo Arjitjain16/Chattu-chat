4a31d94bd9aa604dd6eaa7d97dde5659f66ef6f5
Your Name <you@example.com>
2024-08-31 23:50:37 +0530

    Get message commit

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,14 +1,23 @@
 
+import useGetMessages from '../../hooks/useGetMessages';
 import Message from './Message';
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 function Messages() {
+  const { loading, messages } = useGetMessages()
+  console.log("messages",messages)
   return (
+
     <div className='p-4 flex-1 overflow-auto'>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {!loading && messages.length >0 && messages.map((message) =>(
+        <div key={message._id}>
+          <Message message={message} />
+        </div>
+      ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+			{!loading && messages.length === 0 && (
+				<p className='text-center text-white'>Send a message to start the conversation</p>
+			)}
     </div>
   );
 }
