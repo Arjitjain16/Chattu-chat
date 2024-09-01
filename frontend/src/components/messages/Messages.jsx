b8931d4b4a03a4f992e567a9c8aed5477111263f
Your Name <you@example.com>
2024-09-01 23:54:00 +0530

    listen message commit

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -3,9 +3,11 @@ import useGetMessages from '../../hooks/useGetMessages';
 import Message from './Message';
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import { useEffect, useRef } from 'react';
+import useListenMessages from '../../hooks/useListenMessages';
 
 function Messages() {
   const { loading, messages } = useGetMessages()
+  useListenMessages()
   console.log("messages",messages)
 
   const lastMessageRef = useRef()
