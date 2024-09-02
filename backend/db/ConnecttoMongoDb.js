5a5eece1375d337218c077c65a732cf509c8d67d
Your Name <you@example.com>
2024-09-02 22:54:36 +0530

    final deployment commmit

--- backend/db/ConnecttoMongoDb.js
+++ backend/db/ConnecttoMongoDb.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
 
-const connectToMongoDB = async () => {
+
+const connectToMongoDB = async ()=> {
 	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
+		await mongoose.connect(process.env.MONGO_DB_URI ,{
+		});
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
 	}
+	
 };
 
 export default connectToMongoDB;
